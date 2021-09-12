6df45c255188e7ab18e49b6655d0089b137d5b65
Kevin Rodriguez <87106659+Krcoded@users.noreply.github.com>
2021-09-11 23:25:32 -0400

    saving to local

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Todolist from "./Todolist";
+import uuidv4 from "uuidv4";
+
+const LOCAL_STORAGE_KEY = "todoApp.todos";
+
 function App() {
-  const [todos, setTodos] = useState([
-    { id: 1, name: "Todo 1", complete: false },
-  ]);
+  const [todos, setTodos] = useState([]);
+  const todoNameRef = useRef();
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storedTodos) setTodos(storedTodos);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value;
+    if (name === ``) return;
+
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false }];
+    });
+    todoNameRef.current.value = null;
+  }
   return (
     <>
       <Todolist todos={todos} />
-      <input type="text" />
-      <button> Add Todo</button>
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}> Add Todo</button>
       <button> Clear Completed </button>
       <div> 0 Pending Todo</div>
     </>
